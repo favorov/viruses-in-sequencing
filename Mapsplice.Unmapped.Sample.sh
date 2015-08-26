36981f107ea6826cf43f9d6fa730a768bc178580
Alexander Favorov <favorov@sensi.org>
2015-08-26 09:10:54 +0300

    mkdir -p not to have error

--- Mapsplice.Unmapped.Sample.sh
+++ Mapsplice.Unmapped.Sample.sh
@@ -8,6 +8,9 @@
 module load sharedapps
 module load python2.7/2.7.6
 
+virexbase=$scripthome/Chimeric.Genomes
+mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
+
 folder=${1}
 [[ ! $folder = *\/ ]] && folder=${folder}/
 #if folder is not given with / add it
@@ -19,9 +22,10 @@ echo "started looking for HPV and EBV in folder $folder"
 scripthome=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 touch mapsplice.unmapped.start.timestamp.txt
-virexbase=$scripthome/Chimeric.Genomes
-mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
-mkdir virus-findings
+
+mkdir -p virus-findings
+#if not exists, create, otherwise, do nothing, no error
+
 python2 $mapsplice --fusion-non-canonical -o virus-findings/ -c $virexbase/hg19+HPV+EBV -x $virexbase/hg19+HPV/hg19+hpv+ebv -1 unmapped1.fq -2 unmapped2.fq touch mapsplice.unmapped.stop.timestamp.txt
 
 echo 'done..' 
