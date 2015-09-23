afe4c034d9e64cb3174b2896fcf394d7a429b984
Alexander Favorov <favorov@sensi.org>
2015-09-23 21:48:27 +0300

    changed the reference folder parameter code. Now, it is var or parameter or default ~/viruses-in-RNA-seq

--- Mapsplice.Unmapped.Sample.sh
+++ Mapsplice.Unmapped.Sample.sh
@@ -9,10 +9,6 @@ module load sharedapps
 module load python2.7/2.7.6
 module load bowtie/1.1.1
 
-scripthome=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
-chimereref=$scripthome/Chimeric.Genomes/hg19+HPV+EBV
-indexname=hg19+hpv+ebv
-mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
 
 folder=${folder-$1}
 #folder is to be passed by -v option to qsub or set by other method
@@ -20,9 +16,15 @@ folder=${folder-$1}
 [[ ! $folder = *\/ ]] && folder=${folder}/
 #if folder is not given with / add it
 #folder is the working folder
-
 #folder is to exist on that stage
 
+#scripthome=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
+#it works only if script is started not by qrsh
+scripthome=${scripthome-${2-'~/viruses-in-RNA-seq'}}
+
+chimereref=$scripthome/Chimeric.Genomes/hg19+HPV+EBV
+indexname=hg19+hpv+ebv
+mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
 
 echo "started looking for HPV and EBV in folder $folder"
 
