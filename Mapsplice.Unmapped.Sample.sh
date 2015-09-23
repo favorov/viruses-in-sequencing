4e1429c2f0ed3fd6584dd127091c969ca399154c
Alexander Favorov <favorov@sensi.org>
2015-09-23 21:54:23 +0300

    timestamp name starts with timestamp now

--- Mapsplice.Unmapped.Sample.sh
+++ Mapsplice.Unmapped.Sample.sh
@@ -28,18 +28,18 @@ mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
 
 echo "started looking for HPV and EBV in folder $folder"
 
-if [ ! -f $folder/mapsplice.unmapped.stop.timestamp.txt ]
+if [ ! -f $folder/timestamp.mapsplice.unmapped.stop.txt ]
 then
 	pushd $folder > /dev/null
 	
-	touch mapsplice.unmapped.start.timestamp.txt
+	touch timestamp.mapsplice.unmapped.start.txt
 	
 	mkdir -p virus-findings
 	#if not exists, create, otherwise, do nothing, no error
 
 	python2 $mapsplice --fusion-non-canonical -o virus-findings/ -c $chimereref -x $chimereref/$indexname -1 unmapped1.fq -2 unmapped2.fq 
 
-	touch mapsplice.unmapped.stop.timestamp.txt
+	touch timestamp.mapsplice.unmapped.stop.txt
 
 	echo 'done..' 
 	popd > /dev/null 
