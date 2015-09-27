50715cb4a00df9ffe5c17f39b6cbfc19116bde32
Alexander Favorov <favorov@sensi.org>
2015-09-27 06:54:44 -0400

    commands for first batch

--- Utils.Califano/prepare.dir.sh
+++ Utils.Califano/prepare.dir.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for dir in /home/elana/CalifanoHPVOP/DGay*
 do
-	echo $dir ${dir##*/}
+	name=${dir##*/}
+	echo $dir $name
+	mkdir $name
+	pushd $name
+	ln -s $dir/alignments.sam .
+	popd
 done
