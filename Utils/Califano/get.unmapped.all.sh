7ba5514bcdd391b55769e94cc09cbcc6e67c1c82
Alexander Favorov <favorov@sensi.org>
2015-09-27 16:22:32 -0400

    hope it will work and start all the get.unmapped tasks

--- Utils/Califano/get.unmapped.all.sh
+++ Utils/Califano/get.unmapped.all.sh
@@ -1,1 +1,9 @@
-qsub -v folder=../DGay10-26144 -N get.unmapped.DGay10-26144 ~/viruses-in-RNA-seq/Get.Unmapped.Reads.Sample.sh
+#!/bin/bash
+#we run it from the folder where all the DGay subfoders are
+for dir in DGay*
+do
+	echo $dir 
+	pushd $dir
+	qsub -v folder=. -N get.unmapped.$dir ~/viruses-in-RNA-seq/Get.Unmapped.Reads.Sample.sh
+	popd
+done
