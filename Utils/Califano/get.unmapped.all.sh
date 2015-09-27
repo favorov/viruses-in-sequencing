04c8fec9e530277490b68f2ad0256489827efff4
Alexander Favorov <favorov@sensi.org>
2015-09-27 15:55:17 -0400

    start the scaffold scripts

---
+++ Utils/Califano/get.unmapped.all.sh
@@ -0,0 +1,1 @@
+qsub -v folder=../DGay10-26144 -N get.unmapped.DGay10-26144 ~/viruses-in-RNA-seq/Get.Unmapped.Reads.Sample.sh
---
+++ Utils/Califano/mapslice.all.sh
@@ -0,0 +1,1 @@
+qsub -v folder=../DGay10-26144 -N mapsplice.unmapped.DGay10-26144 ~/viruses-in-RNA-seq/Mapsplice.Unmapped.Sample.sh
