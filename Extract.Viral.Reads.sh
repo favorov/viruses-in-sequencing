1b14941081ba8142513fe29b6583f100bea58fb2
Alexander Favorov <favorov@sensi.org>
2015-10-06 04:28:55 -0400

    debug exchoes

--- Extract.Viral.Reads.sh
+++ Extract.Viral.Reads.sh
@@ -22,9 +22,12 @@ then
 	touch timestamp.extract.viral.reads.start.txt
 	samtools view -Sbh alignments.sam > alignments.bam
 	#samtools view -bh alignments.bam > alignments.bam
-	samtools index alignments.bam 
+	samtools index alignments.bam
+	echo 1
 	samtools sort alignments.bam  alignments.sorted
+	echo 2
 	samtools index alignments.sorted.bam 
+	echo 3
 	samtools view -bh -o viral_reads.bam alignments.sorted.bam hpv16 hpv33 hpv35 hhv4 hhv4t1
 	touch timestamp.extract.viral.reads.stop.txt
 	popd > /dev/null 
