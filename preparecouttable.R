64d3d1e9c03abd5b602d4773ab1d8077ddb46a11
Alexander Favorov <favorov@sensi.org>
2015-10-08 03:11:57 -0400

    typy in gtf filename fixed, name of readgtf function changed to more modern

--- preparecouttable.R
+++ preparecouttable.R
@@ -7,8 +7,8 @@ library(gplots)
 
 bamfiles <- BamFileList(list.files(full.names = T,pattern = 'viral_reads.bam'), yieldSize=2000000)
 
-gtffile <- file.path('~/viruses-in-sequencing/Viral.Genes.Annotation/hpv.gtf') 
-txdb <- makeTranscriptDbFromGFF(gtffile, format="gtf")
+gtffile <- file.path('~/viruses-in-RNA-seq/Viral.Genes.Annotation/hpv.gtf') 
+txdb <- makeTxDbFromGFF(gtffile, format="gtf")
 genes <- exonsBy(txdb, by="gene")
 
 se <- summarizeOverlaps(features=genes, reads=bamfiles, mode="Union", singleEnd=T, ignore.strand=TRUE, fragments=F )
