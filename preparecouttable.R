300acef2db3ac640157955ae5d556b450b403964
Alexander Favorov <favorov@sensi.org>
2015-10-11 04:06:42 -0400

    split dirmane only by /

--- preparecouttable.R
+++ preparecouttable.R
@@ -9,7 +9,7 @@ second<-function(li){li[[2]]}
 
 bamlist<-list.files(full.names = T,pattern = 'viral_reads.bam',recursive=TRUE)
 bamfiles <- BamFileList(bamlist, yieldSize=2000000)
-names(bamfiles)<-sapply(strsplit(sapply(strsplit(bamlist,'/'),second),'-'),second)
+names(bamfiles)<-sapply(strsplit(bamlist,'/'),second)
 gtffile <- file.path('~/viruses-in-RNA-seq/Viral.Genes.Annotation/hpv.gtf') 
 txdb <- makeTxDbFromGFF(gtffile, format="gtf")
 genes <- exonsBy(txdb, by="gene")
