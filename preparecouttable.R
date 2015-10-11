197a1392b3a2016ca5a7e6d04dba238b547bc52c
Alexander Favorov <favorov@sensi.org>
2015-10-11 03:58:46 -0400

    first working version

--- preparecouttable.R
+++ preparecouttable.R
@@ -5,14 +5,14 @@ library(DESeq2)
 library(org.Hs.eg.db)
 library(gplots)
 
-bamfiles <- BamFileList(list.files(full.names = T,pattern = 'viral_reads.bam'), yieldSize=2000000)
+second<-function(li){li[[2]]}
 
+bamlist<-list.files(full.names = T,pattern = 'viral_reads.bam',recursive=TRUE)
+bamfiles <- BamFileList(bamlist, yieldSize=2000000)
+names(bamfiles)<-sapply(strsplit(sapply(strsplit(bamlist,'/'),second),'-'),second)
 gtffile <- file.path('~/viruses-in-RNA-seq/Viral.Genes.Annotation/hpv.gtf') 
 txdb <- makeTxDbFromGFF(gtffile, format="gtf")
 genes <- exonsBy(txdb, by="gene")
-
 se <- summarizeOverlaps(features=genes, reads=bamfiles, mode="Union", singleEnd=T, ignore.strand=TRUE, fragments=F )
-
-head(assay(se))
-
-
+assay(se)
+save(file='hpv_expression.Rda',list=c('se'))
