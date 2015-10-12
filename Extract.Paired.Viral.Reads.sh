0247cc7b244fc9410324b661b9e51679a49acc2b
Alexander Favorov <favorov@sensi.org>
2015-10-12 04:30:59 -0400

    grep-based version, no perl unique id extracter used

--- Extract.Paired.Viral.Reads.sh
+++ Extract.Paired.Viral.Reads.sh
@@ -17,7 +17,7 @@ folder=${folder-$1}
 
 #scripthome=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 #it works only if script is started not by qrsh
-scripthome=${scripthome-${2-~/viruses-in-RNA-seq}}
+#scripthome=${scripthome-${2-~/viruses-in-RNA-seq}}
 
 if [ ! -f ${folder}timestamp.extract.viral.reads.stop.txt ]
 then
@@ -30,14 +30,19 @@ then
 	#it is to exist at that moment
 	touch ../timestamp.extract.paired.viral.reads.start.txt
 	pushd ${folder}virus-findings >/dev/null
-	samtools view -h viral_reads.bam | grep -v -F XF: > viral_reads_no_fus.sam 
-	cat viral_reads_no_fus.sam | cut -f1 | sort > viral_read.ids
+	samtools view viral_reads.bam |  cut -f1 | sort > viral_read.ids
+	#samtools view -h viral_reads.bam | grep -v -F XF: > viral_reads_no_fus.sam 
+	#cat viral_reads_no_fus.sam | cut -f1 | sort > viral_read.ids
 	#we take all the viral reads
-	perl ${scripthome}/kill.ids.pairs.pl viral_read.ids > unpaired_viral_read.ids
+	#perl ${scripthome}/kill.ids.pairs.pl viral_read.ids > unpaired_viral_read.ids
 	#and, find those that do not have a pair
- 	LC_ALL=C grep -w -F -f unpaired_viral_read.ids alignments.sam > unpaired_viral_reads_with_mates.sam
+ 	LC_ALL=C grep -w -F -f viral_read.ids alignments.sam > viral_reads_with_mates_and_fusions.sam
 	#and extract all the reads with this ids form the chimeric-alignment sam file
 	#samtools view -bh -o viral_reads.bam alignments.sorted.bam hpv16 hpv33 hpv35 hhv4 hhv4t1
+	grep -w -F XF: viral_reads_with_mates_and_fusions.sam | cut -f1 | sort | uniq > viral_fusion_read.ids
+	#get fusion read ids
+	grep -v -w -R -f viral_fusion_read.ids viral_reads_with_mates_and_fusions.sam > viral_reads_with_mates.sam
+	#remove lines with the fusion ids 
 	touch ../timestamp.extract.paired.viral.reads.stop.txt
 	popd > /dev/null 
 	echo 'done..'
