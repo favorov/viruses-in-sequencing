baa2a74e66c581ab978c3ae07c0fbf163be906bd
Alexander Favorov <favorov@sensi.org>
2015-10-12 23:31:49 -0400

    we count all reads that are in viral_reads bams, virus-by-virus

--- prepare.counts.from.extracted.viral.reads.R
+++ prepare.counts.from.extracted.viral.reads.R
@@ -14,9 +14,23 @@ gtffile <- file.path('~/viruses-in-RNA-seq/Viral.Genes.Annotation/hpv.gtf')
 txdb <- makeTxDbFromGFF(gtffile, format="gtf")
 genes <- exonsBy(txdb, by="gene")
 counts<-matrix(nrow=0,ncol=length(bamlist))
+
 for (i in 1:length(genes))
 {
 	se<-summarizeOverlaps(features=genes[i], reads=bamfiles, mode="Union", singleEnd=T, ignore.strand=TRUE, fragments=F )
 	counts<-rbind(counts,assay(se))
 }
-save(file='hpv_expression.Rda',list=c('counts'))
+#gtf does not have the whole virus info, is it good not
+list.of.full.counters<-sapply(bamfiles,function(bam){
+	table(as.character(scanBam(bam)[[1]]$rname))
+})
+
+hpv16<-sapply(list.of.full.counters,function(vec) ifelse(is.na(vec['hpv16']),0,vec['hpv16']))
+hpv33<-sapply(list.of.full.counters,function(vec) ifelse(is.na(vec['hpv33']),0,vec['hpv33']))
+hpv35<-sapply(list.of.full.counters,function(vec) ifelse(is.na(vec['hpv35']),0,vec['hpv35']))
+hhv4<-sapply(list.of.full.counters,function(vec) ifelse(is.na(vec['hhv4']),0,vec['hhv4']))
+hhv4t1<-sapply(list.of.full.counters,function(vec) ifelse(is.na(vec['hhv4t1']),0,vec['hhv4t1']))
+
+counts<-rbind(counts,hpv1,hpv33,hpv35,hhv4,hhv4t1)
+
+#save(file='hpv_expression.Rda',list=c('counts'))
