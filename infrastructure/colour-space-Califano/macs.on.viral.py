efc954c8d24567869aca7eae4c1697a704cd42c9
Alexander Favorov <favorov@sensi.org>
2016-03-28 02:15:05 -0400

    dir listing

--- infrastructure/colour-space-Califano/macs.on.viral.py
+++ infrastructure/colour-space-Califano/macs.on.viral.py
@@ -1,19 +1,30 @@
 #!python3
 #requires python3
-#we run it from the folder (fastq in this project) with all the JHUJC subfoders with cfasta are in 
-def main():
-	name='18732'
-	t='JHUJC01001_027_18732_Enrich/raw/viral-reads.sam'
-	c='JHUJC01001_028_18732_Total/raw/viral-reads.sam'
-	command='''
+#we run it from the folder (fastq in this project) with all the JHUJC subfoders with cfasta are in
+import os
+impors glob
+
+def print_command(t,c,name):
+	print('''
 cat << ENDSCRIPT | qsub -N macs.%s -cwd -S /bin/bash -j y
 #!/bin/bash
 . /etc/profile.d/modules.sh
 module load sharedapps
 module load macs 
 macs2 callpeak -t %s -c %s -n %s --nomodel -g 25000 
-ENDSCRIPT''' % (name,t,c,name)
-	print(command) 
+ENDSCRIPT''' % (name,t,c,name))
+
+
+def main():
+    foldernames=glob.glob('JHUJC0*')
+		print foldernames
+    #name='18732'
+    #t='JHUJC01001_027_18732_Enrich/raw/viral-reads.sam'
+    #c='JHUJC01001_028_18732_Total/raw/viral-reads.sam'
+    #print_command(t,c,name)
+
+
+
 #here, we finally run it all :)
 if __name__ == "__main__":
     main()
