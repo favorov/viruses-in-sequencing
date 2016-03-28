4d799800b9fc8a91e1c351d8b8e909f93db17e6f
Alexander Favorov <favorov@sensi.org>
2016-03-27 23:52:53 -0400

    polishing in-doc script

--- infrastructure/colour-space-Califano/macs.on.viral.sh
+++ infrastructure/colour-space-Califano/macs.on.viral.sh
@@ -4,8 +4,10 @@ name=18732
 t=JHUJC01001_027_18732_Enrich/raw/viral-reads.sam
 c=JHUJC01001_028_18732_Total/raw/viral-reads.sam
 cat << ENDSCRIPT | qsub -N macs.$name -cwd -S /bin/bash -j y
-	/etc/profile.d/modules.sh
-	module load sharedapps
-	module load samtools
-	macs2 callpeak -t $t -c $c -n $name --nomodel -g 25000 
-	ENDSCRIPT
+#!/bin/bash
+. /etc/profile.d/modules.sh
+module load sharedapps
+module load samtools
+macs2 callpeak -t $t -c $c -n $name --nomodel -g 25000 
+ENDSCRIPT
+
