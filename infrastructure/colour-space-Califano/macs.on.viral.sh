136c3b83468024db701a665e68dc07a614b89e0e
Alexander Favorov <favorov@sensi.org>
2016-03-27 23:36:48 -0400

    trying in-doc script

--- infrastructure/colour-space-Califano/macs.on.viral.sh
+++ infrastructure/colour-space-Califano/macs.on.viral.sh
@@ -1,1 +1,11 @@
-macs2 callpeak -t JHUJC01001_027_18732_Enrich/raw/viral-reads.sam -c JHUJC01001_028_18732_Total/raw/viral-reads.sam -n 18732 --nomodel -g 25000
+#!/bin/bash
+#we run it from the folder (fastq in this project) with all the JHUJC subfoders with cfasta are in 
+name=18732
+t=JHUJC01001_027_18732_Enrich/raw/viral-reads.sam
+c=JHUJC01001_028_18732_Total/raw/viral-reads.sam
+cat << ENDSCRIPT | qsub -N macs.$name -cwd -S /bin/bash -j y
+	/etc/profile.d/modules.sh
+	module load sharedapps
+	module load samtools
+	macs2 callpeak -t $t -c $c -n $name --nomodel -g 25000 
+	ENDSCRIPT
