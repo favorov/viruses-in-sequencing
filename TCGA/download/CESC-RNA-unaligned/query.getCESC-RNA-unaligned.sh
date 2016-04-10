6ea5d7f3094933a76b02c74a1275217c12390317
Alexander Favorov <favorov@sensi.org>
2016-04-10 07:02:34 +0300

    echo id to look at

--- TCGA/download/CESC-RNA-unaligned/query.getCESC-RNA-unaligned.sh
+++ TCGA/download/CESC-RNA-unaligned/query.getCESC-RNA-unaligned.sh
@@ -4,5 +4,6 @@ rm -f ${manif}/querylog.txt
 touch  ${manif}/querylog.txt
 for ID in `cat samplenames.txt`
 do
+  echo ${ID}
   ~/cghub/bin/cgquery "disease_abbr=CESC&library_strategy=RNA-seq&legacy_sample_id=${ID}*&state=live&refassem_short_name=unaligned" -o ${manif}/${ID}.xml >> ${manif}/querylog.txt
 done
