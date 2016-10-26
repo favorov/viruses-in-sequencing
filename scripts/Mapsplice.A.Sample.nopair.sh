69593b106eb9bd3ea122df1cc9cc7812949b61c9
Alexander Favorov <favorov@sensi.org>
2016-10-26 02:46:19 -0400

    single fasta script, maybe work

--- scripts/Mapsplice.A.Sample.nopair.sh
+++ scripts/Mapsplice.A.Sample.nopair.sh
@@ -12,9 +12,10 @@ module load python2.7/2.7.6
 module load bowtie/1.1.1
 
 
-folder=${folder-$1}
-#folder is to be passed by -v option to qsub or set by other method
+fasta=${fasta-$1}
+#fasta is to be passed by -v option to qsub or set by other method
 #if it is not, we try ${1}
+fasta=${folder-${2-'.'}}
 #[[ ! $folder = *\/ ]] && folder=${folder}/
 #if folder is not given with / add it
 #folder is the working folder
@@ -22,26 +23,26 @@ folder=${folder-$1}
 
 #scripthome=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 #it works only if script is started not by qrsh
-scripthome=${scripthome-${2-~/viruses-in-sequencing/}}
+scripthome=${scripthome-${3-'~/viruses-in-sequencing/'}}
 
-chimereref=${scripthome}/genomes/Chimeric.Genomes/hg19+HPV.common
-indexname=hg19+hpv
+chimereref=${scripthome}/genomes/Chimeric.Genomes/hg19+HPV16.common
+indexname=hg19+hpv16
 mapsplice=~/Mapsplice/MapSplice-v2.2.0/mapsplice.py
 
 echo "started looking for various common HPV in folder $folder"
 
-if [ ! -f $folder/timestamp.mapsplice.unmapped.stop.txt ]
+if [ ! -f $folder/timestamp.mapsplice.${fasta}.stop.txt ]
 then
 	pushd $folder > /dev/null
 	
-	touch timestamp.mapsplice.unmapped.start.txt
+	touch timestamp.mapsplice.${fasta}.start.txt
 	
 	mkdir -p virus-findings
 	#if not exists, create, otherwise, do nothing, no error
 
-	python2 $mapsplice --fusion --fusion-non-canonical -o virus-findings/ -c $chimereref -x $chimereref/$indexname -1 unmapped1.fq -2 unmapped2.fq 
+	python2 $mapsplice --fusion --fusion-non-canonical -o virus-findings/ -c $chimereref -x $chimereref/$indexname -1 ${fasta} 
 
-	touch timestamp.mapsplice.unmapped.stop.txt
+	touch timestamp.mapsplice.${fasta}.stop.txt
 
 	echo 'done..' 
 	popd > /dev/null 
