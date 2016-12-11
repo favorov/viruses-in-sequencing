5bb8a8224dbd036c25d0042403aad7fb89e956c8
Alexander Favorov <favorov@sensi.org>
2016-12-11 06:45:38 -0500

    filters

--- vinseq/R/plot.hist.sam.reads.R
+++ vinseq/R/plot.hist.sam.reads.R
@@ -1,5 +1,16 @@
 library(Rsamtools)
-reads.geom.hist<-function(fname,hist.title=fname){
+
+#filters return logical vetor of the 
+#same lenth as sam
+all.good.filter<-function(sam){
+	rep(TRUE,length(sam[[1]]))
+}
+
+hpv16.filter<-function(sam){
+	sam$rname=='hpv16'
+}
+
+reads.geom.hist<-function(fname,hist.title=fname,filter=all.good.filter){
 #name of sam file, no extesion
   len<-nchar(fname)
   if(tolower(substr(fname,len-3,len)=='.sam'))
@@ -19,9 +30,11 @@ reads.geom.hist<-function(fname,hist.title=fname){
 	if (!file.exists(bamname)) stop(paste0("No bam file ",bamname))
 	reads<-scanBam(bamname)
 	pdf(paste0(fname,'.pdf'))
-	hist(reads[[1]]$pos,main=paste0(hist.title,': starts'))
-	hist(reads[[1]]$pos+reads[[1]]$qwidth,main=paste0(hist.title,': ends'))
-	hist(reads[[1]]$qwidth,main=paste0(hist.title,': lenghts'))
+	flt<-filter(reads[[1]])
+	hist(reads[[1]]$pos[flt],main=paste0(hist.title,': starts'))
+	hist(reads[[1]]$pos[flt]+reads[[1]]$qwidth[flt],
+		main=paste0(hist.title,': ends'))
+	hist(reads[[1]]$qwidth[flt],main=paste0(hist.title,': lenghts'))
 	dev.off()
 }
 
