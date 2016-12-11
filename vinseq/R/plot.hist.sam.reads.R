ae85eff0458c59aabbbb42d3d5211d767f7c9506
Alexander Favorov <favorov@sensi.org>
2016-12-11 05:49:26 -0500

    filename can be with .sam or without

--- vinseq/R/plot.hist.sam.reads.R
+++ vinseq/R/plot.hist.sam.reads.R
@@ -1,13 +1,19 @@
 library(Rsamtools)
 reads.geom.hist<-function(fname,hist.title=fname){
 #name of sam file, no extesion
-	samname<-paste0(fname,'.sam')
+  len<-nchar(fname)
+  if(tolower(substr(fname,len-3,len)=='.sam'))
+    fname=substr(fname,1,len-4)
+  samname<-paste0(fname,'.sam')
 	bamname<-paste0(fname,'.bam')
 	if (
 		file.exists(samname)
 		&&
 		!file.exists(bamname)
-	) asBam(samname,fname,overwrite=TRUE)
+	) {
+	  message(samname)
+	  asBam(samname,fname,overwrite=TRUE)
+	}
 	if (!file.exists(bamname)) stop(paste0("No bam file ",bamname))
 	reads<-scanBam(bamname)
 	pdf(paste0(fname,'.pdf'))
