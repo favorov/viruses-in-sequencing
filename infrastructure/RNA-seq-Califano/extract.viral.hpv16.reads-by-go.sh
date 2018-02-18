6b0f338c3948e28c2e77ac69458092ed459d7ee7
Alexander Favorov <favorov@sensi.org>
2018-02-18 12:39:22 +0300

    simple infrastructure for hpv extrator in go

---
+++ infrastructure/RNA-seq-Califano/extract.viral.hpv16.reads-by-go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#we run it from the folder where all the DGay subfoders are
+for dir in DGay*
+do
+	echo $dir 
+	pushd $dir
+	command="~/viruses-in-sequencing/go-scripts/hpv16samfilter"
+	echo "#$ -S /bin/bash" > command.sh
+	chmod 755 command.sh
+	echo $command >> command.sh
+	qsub -N go.extract.hpv16.$dir -j y -cwd command.sh
+	#cat "go run ~/viruses-in-sequencing/go-scripts/hpv16samfilter" | qsub -v folder=. -N go.extract.hpv16.$dir 
+	#rm command.sh
+	popd
+done
