87a8bbf5b487df725f35c49b1dbecea0abb55dbc
Alexander Favorov <favorov@sensi.org>
2018-02-18 13:13:26 +0300

    fixed typo, test version now

--- infrastructure/RNA-seq-Califano/extract.viral.hpv16.reads-by-go.sh
+++ infrastructure/RNA-seq-Califano/extract.viral.hpv16.reads-by-go.sh
@@ -2,14 +2,14 @@
 #we run it from the folder where all the DGay subfoders are
 for dir in DGay*
 do
-	dir=${dir}/virus-findings
-	echo $dir 
-	pushd $dir
+	dirs=${dir}/virus-findings
+	echo $dirs 
+	pushd $dirs
 	command="~/viruses-in-sequencing/go-scripts/hpv16samfilter"
 	echo "#$ -S /bin/bash" > command.sh
 	chmod 755 command.sh
 	echo $command >> command.sh
-	qsub -N go.extract.hpv16.$dir -j y -cwd command.sh
+	#qsub -N go.extract.hpv16.$dir -j y -cwd command.sh
 	#cat "go run ~/viruses-in-sequencing/go-scripts/hpv16samfilter" | qsub -v folder=. -N go.extract.hpv16.$dir 
 	#rm command.sh
 	popd
