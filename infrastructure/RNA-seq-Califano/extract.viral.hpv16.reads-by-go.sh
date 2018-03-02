52aa2b761a170d497eab72174c651f59c8d359e8
Alexander Favorov <favorov@sensi.org>
2018-03-03 02:45:56 +0300

    We moved HPV-related stuff to the HPV folder. Added first json from 7b, it does not work :)

--- infrastructure/RNA-seq-Califano/extract.viral.hpv16.reads-by-go.sh
+++ infrastructure/RNA-seq-Califano/extract.viral.hpv16.reads-by-go.sh
@@ -2,6 +2,7 @@
 #we run it from the folder where all the DGay subfoders are
 for dir in DGay*
 do
+	echo $dir
 	dirs=${dir}/virus-findings
 	echo $dirs 
 	pushd $dirs
@@ -9,7 +10,7 @@ do
 	echo "#$ -S /bin/bash" > command.sh
 	chmod 755 command.sh
 	echo $command >> command.sh
-	#qsub -N go.extract.hpv16.$dir -j y -cwd command.sh
+	qsub -N go.extract.hpv16.$dir -j y -cwd command.sh
 	#cat "go run ~/viruses-in-sequencing/go-scripts/hpv16samfilter" | qsub -v folder=. -N go.extract.hpv16.$dir 
 	#rm command.sh
 	popd
