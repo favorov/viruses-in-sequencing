4eabc7e4b181cb512837a7cae8248ac7c77bd9fe
Alexander Favorov <favorov@sensi.org>
2018-11-17 15:25:03 -0500

    table made more readable, hpv coordinates firsta and chr second

--- infrastructure/RNA-seq-Califano/integration.hpv.table.with.geometry.sh
+++ infrastructure/RNA-seq-Califano/integration.hpv.table.with.geometry.sh
@@ -9,10 +9,20 @@ do
 			fusions_file=$sample/virus-findings/fusions_${type}.txt
 			if grep --quiet $virus ${fusions_file}
 			then
-				prefix="${sample} $type ${virus}"
-				grep ${virus} ${fusions_file} | cut -f1,2,3 | awk -v pre="$prefix" '{gsub(/\t/, "  "); print pre,$_ }' 
+				prefix="${sample} $type"
+				grep ${virus} ${fusions_file} | cut -f1,2,3 | \
+					awk -v pre="$prefix" '
+						{
+							gsub(/\t/, "  ");
+							split($1,a,"~"); 
+						}
+						a[1]~/hpv/ {print pre,$1,a[1],$2,a[2],$3}
+						a[2]~/hpv/ {print pre,$1,a[2],$3,a[1],$2}
+					' 
 			fi
 		done
+		exit 
+		# devel
 	done
 done
 
