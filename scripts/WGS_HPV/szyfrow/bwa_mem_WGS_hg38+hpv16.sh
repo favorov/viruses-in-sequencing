52bc90852128fdfdba9896f0b263b0564bae8930
Alexander Favorov <favorov@sensi.org>
2020-05-10 02:38:11 -0400

    first version to run on szyfrow

--- scripts/WGS_HPV/szyfrow/bwa_mem_WGS_hg38+hpv16.sh
+++ scripts/WGS_HPV/szyfrow/bwa_mem_WGS_hg38+hpv16.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#$ -cwd
 
 #echo current folder
 echo "current folder is $PWD"
 
 
-WGSdir="/mnt/mapr/group/ms-solid/DashaHNSCC/"
-reference="/home/atgc/METHYLATION/HPV_cancer/RefGenomes/chimericHG38.fa"
-outputdir="/mnt/mapr/group/ms-solid/DashaHNSCC/8521512003336/test_bwa_mem_JHU/bwa_mem_res/"
+WGSdir="/home/favorov/Califano/WGS/F15FTSUSAT0806_HUMbxtR/"
+reference="/home/favorov/reference/chimeric_hg38+hpv16/bwa_index/chimericHG38.fa"
+outputdir="/home/favorov/Dasha/JHU-WGS-HPV/chimeric-bams/"
 list=('8521512003243' '8521512003248' '8521512003263' '8521512003271' '8521512003274' '8521512003277' '8521512003280' '8521512003281' '8521512003282' '8521512003284' '8521512003288' '8521512003289' '8521512003292' '8521512003293' '8521512003298' '8521512003300' '8521512003301' '8521512003311' '8521512003313' '8521512003318' '8521512003323' '8521512003328' '8521512003330' '8521512003333' '8521512003336' '8521512003337' '8521512003338' '8521512003339' '8521512003340' '8521512003341')
 
 
@@ -25,8 +24,8 @@ for sample in ${list[@]}
 	#run bwa mem for each pair in sample
 	for wgs1 in "${wgsfiles[@]}"
 		do
-		echo "qsub  -o $outputdir -e $outputdir run_bwa_mem_WGS_JHU.sh $wgs1 $iter $outputdir $reference $sample"
-		qsub  -o $outputdir -e $outputdir -v wgs1=$wgs1,iter=$iter,outputdir=$outputdir,reference=$reference,sample=$sample run_bwa_mem_WGS_JHU.sh
+		echo "qsub  -o $outputdir -e $outputdir run_bwa_mem_WGS_hg38+hpv16.sh $wgs1 $iter $outputdir $reference $sample"
+		qsub  -o $outputdir -e $outputdir -v wgs1=$wgs1,iter=$iter,outputdir=$outputdir,reference=$reference,sample=$sample run_bwa_mem_WGS_hg38+hpv16.sh	
 		((iter++))
 		done
 	done
