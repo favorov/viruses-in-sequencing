ed0c4175bcad67dfa0033f90a5c6102fd0f622d2
Alexander Favorov <favorov@sensi.org>
2020-05-11 03:04:09 -0400

    added module load; commented samtools index

--- scripts/WGS_HPV/szyfrow/run_bwa_mem_WGS_hg38+hpv16.sh
+++ scripts/WGS_HPV/szyfrow/run_bwa_mem_WGS_hg38+hpv16.sh
@@ -3,6 +3,8 @@
 #$ -cwd
 #$ -pe zappa-pe 8
 
+module load bwa
+module load samtools
 echo "qsub  -o $outputdir -e $outputdir run_bwa_mem_WGS_hg38+hpv16.sh $wgs1 $iter $outputdir $reference $sample"
 pe=4
 
@@ -10,7 +12,7 @@ wgs2=${wgs1/_1.fq/_2.fq}
 outwgs="${outputdir}${iter}_${sample}.bam"
 echo "bwa mem -t $pe -T 20 $reference $wgs1 $wgs2 | samtools sort -@$pe -o $outwgs -"
 bwa mem -t $pe -T 20 $reference $wgs1 $wgs2 | samtools sort -@$pe -o $outwgs -
-samtools index $outwgs
+#samtools index $outwgs
 #outwgsHPV=${outwgs/'.bam'/'.HPV.sam'}
 #samtools view -H $outwgs > $outwgsHPV
 #samtools view $outwgs | grep "NC_001526" >> $outwgsHPV
