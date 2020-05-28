313c1e2d1bd0eca18e9c3818a3f70d07262393c2
Alexander Favorov <favorov@sensi.org>
2020-05-28 05:04:30 -0400

    changed bam folder

--- py-scripts/extract_HPV_reads_and_their_mates_from_BAM.py
+++ py-scripts/extract_HPV_reads_and_their_mates_from_BAM.py
@@ -1,6 +1,7 @@
-#$ -S /usr/bin/env python3
+#$ -S python3
 #$ -cwd
-#$ -pe smp 8
+##$ -pe zappa-pe 8
+#$ 
 
 import os
 import sys
@@ -59,13 +60,11 @@ def grepMates(infile,outfile):
 
 
 def main():
-	WGSdir="/mnt/mapr/group/ms-solid/DashaHNSCC/"
-	reference="/home/atgc/METHYLATION/HPV_cancer/RefGenomes/chimericHG38.fa"
-	outputdir="/mnt/mapr/group/ms-solid/DashaHNSCC/8521512003336/test_bwa_mem_JHU/bwa_mem_res/"
+	#bamdir="/mnt/mapr/group/ms-solid/DashaHNSCC/8521512003336/test_bwa_mem_JHU/bwa_mem_res/"
+	bamdir=/home/favorov/Dasha/JHU-WGS-HPV/chimeric-bams/
 	samples=['8521512003243','8521512003248','8521512003263','8521512003271','8521512003274','8521512003277','8521512003280','8521512003281','8521512003282','8521512003284','8521512003288','8521512003289','8521512003292','8521512003293','8521512003298','8521512003300','8521512003301','8521512003311','8521512003313','8521512003318','8521512003323','8521512003328','8521512003330','8521512003333','8521512003336','8521512003337','8521512003338','8521512003339','8521512003340','8521512003341']
-	pe=8
 	for sample in samples:
-		list=glob.glob(outputdir+"*"+sample+'.bam')
+		list=glob.glob(bamdir+"*"+sample+'.bam')
 		if not list:
 			print("No BAM files for "+sample+" sample")
 		else:
